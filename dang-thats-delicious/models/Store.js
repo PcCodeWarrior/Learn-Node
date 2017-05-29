1d38f06bc60a5f9409faa207db737c0f278fe89e
pccodewarrior <pc_code_warrior@yahoo.com>
2017-05-29 14:52:27 -0600

    add tag highlights

--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -45,5 +45,16 @@ storeSchema.pre('save',function(next){
   this.slug=slug(this.name);next();
   next();
 });
+
+
+storeSchema.statics.getTagsList= function(){
+  return this.aggregate([
+      {$unwind: '$tags'},
+      {$group: { _id: '$tags', count: {$sum: 1}}},
+      {$sort: {count: -1 }}
+  ]);
+};
+
+
 module.exports= mongoose.model('Store', storeSchema);
 
