46251a1f03c7db3f9d068a1547d3d1cc17121d52
PcCodeWarrior <PC_Code_Warrior@yahoo.com>
2017-06-15 11:59:27 -0600

    Pushing UserData to API

--- dang-thats-delicious/public/javascripts/delicious-app.js
+++ dang-thats-delicious/public/javascripts/delicious-app.js
@@ -7,6 +7,7 @@ import { $, $$ } from './modules/bling';
 import autocomplete from './modules/autocomplete';
 import typeAhead from './modules/typeAhead';
 import makeMap from './modules/map';
+import ajaxHeart from './modules/heart';
 
 
 autocomplete($('#address'), $('#lat'), $('#lng'));
@@ -14,4 +15,8 @@ autocomplete($('#address'), $('#lat'), $('#lng'));
 typeAhead( $('.search'));
 
 makeMap( $('#map'));
+
+const heartForms = $$('form.heart');
+heartForms.on('submit',ajaxHeart);
+
 
